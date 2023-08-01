3a2e15c21c683a5fafb50600ed263761ae2430fe
lesantosx <leticia.santos1997@outlook.com>
2023-08-01 19:10:45 -0300

    Adjust footer and cards

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,20 +1,20 @@
 import styles from './Footer.module.scss'
-// import { ReactComponent as Registered } from 'assets/marca_registrada.svg'
-
 import icons from 'json/icons-social.json'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div>
-        <span>Developed by lesantosx</span>        
-          {icons.map((icon) => {
-            return (              
-                <a key={icon.name} href={icon.link} target="_blank" rel="noreferrer">
-                  <img src={icon.icon} alt={icon.name} />
-                </a>
-            )
-          })}        
+        <span>Developed by lesantosx</span>       
+      </div>
+      <div>
+        {icons.map((icon) => {
+          return (              
+              <a key={icon.name} href={icon.link} target="_blank" rel="noreferrer">
+                <img src={icon.icon} alt={icon.name} />
+              </a>
+          )
+        })}  
       </div>
       
     </footer>
--- src/views/Projects/index.jsx
+++ src/views/Projects/index.jsx
@@ -12,8 +12,12 @@ export default function Projects() {
 
   const [projects, setProjects] = useState(projectsJson)
 
-  const filterProjects = (type) => {
-    const newItems = projects.filter((p) => p.type === type)
+  const [filterActive, setFilterActive] = useState('all')
+
+  const filterProjects = (filter) => {
+    const newItems = projects.filter((p) => p.type === filter.id)
+    
+    setFilterActive(filter.type)
 		setProjects(newItems)
   }
 
@@ -21,45 +25,50 @@ export default function Projects() {
     <main className={styles.container}>
       <center className={styles.filters}>       
         <ul className={styles.filters__ul} >      
-          <li key="all" onClick={() => setProjects(projectsJson)}>All</li>
+          <li key="all" className={filterActive === 'all' ? styles.filter__active :  ''} onClick={() => setProjects(projectsJson)}>All</li>
           {filters.map((filter) => {
             return (              
-              <li key={filter.id} onClick={() => filterProjects(filter.id)}>{filter.type}</li>
+              <li key={filter.id} className={filterActive === filter.type ? styles.filter__active :  ''} onClick={() => filterProjects(filter)}>{filter.type}</li>
             )
           })}
         </ul>
       </center>
 
-      <div className={styles.projects}>        
-        {projects.map((project) => {
-          return (
-            <div className={styles.project}>
-              <div>
-                <span className={styles.project__name}>{project.title}</span>
-                <div className={styles.project__skills}>
-                  <ul className={styles.techs}>
-                    {project.techs.map((tech) => {
-                      return (
-                        <li className={styles.techs__li} key={tech.id}>
-                          <Tooltip title={tech.name}>
-                            <img src={tech.icon} alt={tech.name}/>
-                          </Tooltip>
-                          
-                        </li>
-                      )
-                    })}        
-                  </ul>
-                </div>
-                <span className={styles.project__description}>{project.description}</span>
-                <div className={styles.project__btns}>
-                  <a className={styles.btnRead} href={project.preview} target="_blank" rel="noreferrer"><LaptopIcon sx={{ fontSize: 16 }} className={styles.icons}/>live demo</a>
-                  <a className={styles.btnRead} href={project.code} target="_blank" rel="noreferrer"><GitHubIcon sx={{ fontSize: 16 }} className={styles.icons} />source code</a>      
-                </div>
-              </div>            
-            </div>               
-          )
-        })}
-      </div>      
+      {projects.length > 0 ?
+        <div className={styles.projects}>        
+          {projects.map((project) => {
+            return (
+              <div className={styles.project}>
+                <div>
+                  <span className={styles.project__name}>{project.title}</span>
+                  <div className={styles.project__skills}>
+                    <ul className={styles.techs}>
+                      {project.techs.map((tech) => {
+                        return (
+                          <li className={styles.techs__li} key={tech.id}>
+                            <Tooltip title={tech.name}>
+                              <img src={tech.icon} alt={tech.name}/>
+                            </Tooltip>                            
+                          </li>
+                        )
+                      })}        
+                    </ul>
+                  </div>
+                  <span className={styles.project__description}>{project.description}</span>
+                  <div className={styles.project__btns}>
+                    <a className={styles.btnRead} href={project.preview} target="_blank" rel="noreferrer"><LaptopIcon sx={{ fontSize: 16 }} className={styles.icons}/>live demo</a>
+                    <a className={styles.btnRead} href={project.code} target="_blank" rel="noreferrer"><GitHubIcon sx={{ fontSize: 16 }} className={styles.icons} />source code</a>      
+                  </div>
+                </div>            
+              </div>               
+            )
+          })}
+        </div>     
+      
+        : <center className={styles.noProject}>There are no such projects</center>
+      }
+
+            
     </main>
   )
 }
